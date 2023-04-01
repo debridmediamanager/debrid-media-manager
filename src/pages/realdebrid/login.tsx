ada93b15d44fcac59426da7056f42111f12a0dd6
Ben Sarmiento <ben.sarmiento@forto.com>
2023-04-01 12:57:34 +0200

    Debrid Media Manager

--- src/pages/realdebrid/login.tsx
+++ src/pages/realdebrid/login.tsx
@@ -15,7 +15,7 @@ export default function RealDebridLoginPage() {
 				className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
 				onClick={handleAuthorize}
 			>
-				Authorize Debrid Torrent Manager
+				Authorize Debrid Media Manager
 			</button>
 		</div>
 	);
