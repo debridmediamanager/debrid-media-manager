feecc8a2f06185c7c2d8e84e22869a55c9d2f185
Ben Sarmiento <ben.sarmiento@forto.com>
2023-04-10 00:12:04 +0200

    logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,17 @@ function IndexPage() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen">
 			<Toaster position="top-right" />
+			<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
+				<rect x="25" y="25" width="150" height="150" fill="#2C3E50" rx="20" ry="20" />
+				<circle cx="100" cy="100" r="60" fill="#00A0B0" />
+				<path d="M85,65 L85,135 L135,100 Z" fill="#ECF0F1" />
+				<path d="M60,90 Q80,60 100,90 T140,90" fill="#CC333F" />
+				<path
+					d="M75,121 L80,151 L90,136 L100,151 L110,136 L120,151 L125,121 Z"
+					fill="#EDC951"
+				/>
+			</svg>
+			{/* this is made by ChatGPT */}
 			{user ? (
 				<>
 					<h1 className="text-2xl font-bold mb-4">Debrid Media Manager</h1>
