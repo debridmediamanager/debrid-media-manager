35d608cfa605f90e03505ed9d7981f7a3f4ae9b2
Ben Sarmiento <ben.sarmiento@forto.com>
2023-04-12 03:10:26 +0200

    search anon

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -94,8 +94,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	const client = axios.create({
 		httpAgent: agent,
 		headers: {
-			'User-Agent':
-				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+			authority: 'btdig.com',
+			accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+			'accept-language': 'en-US,en;q=0.9',
+			dnt: '1',
+			referer: 'https://btdig.com/',
+			'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-ch-ua-platform': '"macOS"',
+			'sec-fetch-dest': 'document',
+			'sec-fetch-mode': 'navigate',
+			'sec-fetch-site': 'same-origin',
+			'sec-fetch-user': '?1',
+			'upgrade-insecure-requests': '1',
+			'user-agent':
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
 		},
 		timeout: 2000,
 	});
