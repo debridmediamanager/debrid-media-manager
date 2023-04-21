b48bef3a01cb79ce74428dc4cce9aa414a750d3e
Ben Sarmiento <ben.sarmiento@forto.com>
2023-04-21 12:02:44 +0200

    Enable for local testing

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: false,
 	publicRuntimeConfig: {
 		// Will be available on both server and client
+		externalSearchApiHostname: 'https://debridmediamanager.com',
 		realDebridHostname: 'https://corsproxy.org/?https://api.real-debrid.com',
 		allDebridHostname: 'https://api.alldebrid.com',
 		allDebridAgent: 'debridMediaManager',
