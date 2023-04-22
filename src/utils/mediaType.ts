4459e6a3fccdf24e5ab5315d7b9af5fd6d42bc2b
Ben Sarmiento <ben.sarmiento@forto.com>
2023-04-22 10:43:46 +0200

    fix media ident function

--- src/utils/mediaType.ts
+++ src/utils/mediaType.ts
@@ -1,5 +1,5 @@
 export const getMediaType = (filename: string): 'tv' | 'movie' => {
-	return /[\(\.\s)]s\d\d[\be]|[\(\.\s)]season[\.\s]?\d[\d]?[\be]/i.test(filename)
+	return /[\(\.\s)]s\d\d[\be]?|[\(\.\s)]season[\.\s]?\d[\d]?[\be]?/i.test(filename)
 		? 'tv'
 		: 'movie';
 };
