f77da7f2669adb3cad933ffbdb792af2886bb6b9
Ben Adrian Sarmiento <me@bensarmiento.com>
2023-04-24 01:08:29 +0200

    2.4.0 (#28)
    * update todo
    
    * add search speed

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -4,8 +4,8 @@ import {
 	flattenAndRemoveDuplicates,
 	groupByParsedTitle,
 	SearchResult,
+	searchSpeedType,
 } from '@/services/btdigg';
-import { RedisCache } from '@/services/cache';
 import { cleanSearchQuery, getLibraryTypes } from '@/utils/search';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { SocksProxyAgent } from 'socks-proxy-agent';
@@ -16,13 +16,12 @@ export type SearchApiResponse = {
 };
 
 const agent = new SocksProxyAgent(process.env.PROXY!, { timeout: 3000 });
-const cache = new RedisCache();
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<SearchApiResponse>
 ) {
-	const { search, libraryType } = req.query;
+	const { search, libraryType, searchSpeedPassword, searchSpeed } = req.query;
 
 	if (!search || search instanceof Array) {
 		res.status(400).json({ errorMessage: 'Missing "search" query parameter' });
@@ -34,14 +33,27 @@ export default async function handler(
 		return;
 	}
 
+	if (searchSpeed instanceof Array) {
+		res.status(400).json({ errorMessage: 'Missing "searchSpeed" query parameter' });
+		return;
+	}
+
 	const finalQuery = cleanSearchQuery(search);
 	const libraryTypes = getLibraryTypes(libraryType);
 	const client = createAxiosInstance(agent);
 
+	let speed: searchSpeedType = 'veryfast';
+	if (process.env.SEARCH_SPEED_PASSWORD) {
+		speed =
+			searchSpeedPassword === process.env.SEARCH_SPEED_PASSWORD
+				? (searchSpeed as searchSpeedType)
+				: speed;
+	}
+
 	try {
 		const results = [];
 		for (const lType of libraryTypes) {
-			results.push(await fetchSearchResults('fast', client, finalQuery, lType, cache));
+			results.push(await fetchSearchResults(speed, client, finalQuery, lType));
 		}
 		let processedResults = flattenAndRemoveDuplicates(results);
 		if (processedResults.length) processedResults = groupByParsedTitle(processedResults);
