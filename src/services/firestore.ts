4802c3555519c3b2d96d02a8695a94bd0ebeb35d
Ben Sarmiento <ben.sarmiento@forto.com>
2023-08-26 13:41:58 +0200

    Support for firestore as cache

---
+++ src/services/firestore.ts
@@ -0,0 +1,65 @@
+import { Firestore, Settings } from '@google-cloud/firestore';
+
+const firebaseSettings: Settings = {
+	projectId: process.env.FIREBASE_PROJECT_ID, // 'debrid-media-manager',
+	credentials: {
+		client_email: process.env.FIREBASE_CLIENT_EMAIL, // 'client@example.com',
+		private_key: process.env.FIREBASE_PRIVATE_KEY, // '-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----\n'
+	},
+	ssl: true,
+	maxIdleChannels: 2,
+	ignoreUndefinedProperties: true,
+	preferRest: false,
+};
+
+export class FirestoreCache {
+	private db: Firestore;
+
+	constructor() {
+		this.db = new Firestore(firebaseSettings);
+	}
+
+	public async cacheJsonValue<T>(key: string[], value: T) {
+		const sortedKey = key.sort();
+		const firestoreKey = sortedKey.join(':');
+
+		await this.db.runTransaction(async (t) => {
+			const counterDoc = this.db.doc('counters/cache');
+			const counterSnap = await t.get(counterDoc);
+			const newCount = (counterSnap.data()?.count || 0) + 1;
+
+			t.set(this.db.collection('cache').doc(firestoreKey), { value });
+			t.set(counterDoc, { count: newCount });
+		});
+	}
+
+	public async getCachedJsonValue<T>(key: string[]): Promise<T | undefined> {
+		const sortedKey = key.sort();
+		const firestoreKey = sortedKey.join(':');
+		const doc = await this.db.collection('cache').doc(firestoreKey).get();
+		if (!doc.exists) {
+			return undefined;
+		} else {
+			return doc.data()?.value as T;
+		}
+	}
+
+	public async deleteCachedJsonValue(key: string[]): Promise<void> {
+		const sortedKey = key.sort();
+		const firestoreKey = sortedKey.join(':');
+
+		await this.db.runTransaction(async (t) => {
+			const counterDoc = this.db.doc('counters/cache');
+			const counterSnap = await t.get(counterDoc);
+			const newCount = (counterSnap.data()?.count || 0) - 1;
+
+			t.delete(this.db.collection('cache').doc(firestoreKey));
+			t.set(counterDoc, { count: newCount });
+		});
+	}
+
+	public async getDbSize(): Promise<number> {
+		const counterDoc = await this.db.doc('counters/cache').get();
+		return counterDoc.data()?.count || 0;
+	}
+}
