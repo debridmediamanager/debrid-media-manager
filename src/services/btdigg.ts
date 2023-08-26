511c29c008bfa49a5aa7928312adb00b8a27ab9b
Ben Sarmiento <ben.sarmiento@forto.com>
2023-08-26 14:52:24 +0200

    increase timeout

--- src/services/btdigg.ts
+++ src/services/btdigg.ts
@@ -43,7 +43,7 @@ export const createAxiosInstance = (agent: SocksProxyAgent) => {
 			'user-agent':
 				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
 		},
-		timeout: 3000,
+		timeout: 30000,
 	});
 };
 
