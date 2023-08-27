f20ec3d279a9e631198e7217ee20e0bfa149db7c
Ben Sarmiento <ben.sarmiento@forto.com>
2023-08-27 09:23:46 +0200

    no more redis

--- src/services/btdigg.ts
+++ src/services/btdigg.ts
@@ -5,6 +5,7 @@ import { filenameParse, ParsedFilename } from '@ctrl/video-filename-parser';
 import axios, { AxiosInstance } from 'axios';
 import { SocksProxyAgent } from 'socks-proxy-agent';
 import { PlanetScaleCache } from './planetscale';
+import UserAgent from 'user-agents';
 
 export type SearchResult = {
 	title: string;
@@ -27,21 +28,17 @@ export const createAxiosInstance = (agent: SocksProxyAgent) => {
 	return axios.create({
 		httpAgent: agent,
 		headers: {
-			authority: 'btdig.com',
-			accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-			'accept-language': 'en-US,en;q=0.9',
-			dnt: '1',
-			referer: 'https://btdig.com/',
-			'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-			'sec-ch-ua-mobile': '?0',
-			'sec-ch-ua-platform': '"macOS"',
+			accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+			'accept-language': 'en-US,en;q=0.5',
+			'accept-encoding': 'gzip, deflate, br',
+			referer: 'http://btdigggink2pdqzqrik3blmqemsbntpzwxottujilcdjfz56jumzfsyd.onion/',
+			connection: 'keep-alive',
 			'sec-fetch-dest': 'document',
 			'sec-fetch-mode': 'navigate',
 			'sec-fetch-site': 'same-origin',
 			'sec-fetch-user': '?1',
 			'upgrade-insecure-requests': '1',
-			'user-agent':
-				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+			'user-agent': new UserAgent().toString(),
 		},
 		timeout: parseInt(process.env.REQUEST_TIMEOUT || '3000', 10),
 	});
@@ -170,10 +167,15 @@ export async function fetchSearchResults(
 					break;
 				} catch (error: any) {
 					console.error(`Error scraping page ${pageNum} (${finalQuery})`, error.message);
-					retries++;
-					if (retries > MAX_RETRIES) {
-						console.error(`Max retries reached (${MAX_RETRIES}), aborting search`);
-						throw error;
+					if (error.message.includes('status code 429') || error.message.includes('"socket" was not created')) {
+						console.log('Waiting 5 seconds before retrying...');
+						await new Promise((resolve) => setTimeout(resolve, 5000));
+					} else {
+						retries++;
+						if (retries > MAX_RETRIES) {
+							console.error(`Max retries reached (${MAX_RETRIES}), aborting search`);
+							throw error;
+						}
 					}
 					const delay = RETRY_DELAY * Math.pow(2, retries - 1);
 					await new Promise((resolve) => setTimeout(resolve, delay));
