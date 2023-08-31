d1d9fddf828b1e94785a16f3e341858ef8bb7530
Ben Sarmiento <ben.sarmiento@forto.com>
2023-08-31 23:35:49 +0200

    unbreakable tor

--- renew-tor.sh
+++ renew-tor.sh
@@ -5,6 +5,9 @@ ONION_URL="http://btdigggink2pdqzqrik3blmqemsbntpzwxottujilcdjfz56jumzfsyd.onion
 GREP_PATTERN="Histats"
 TOR_COMMAND_PORT="127.0.0.1 9051"
 SLEEP_INTERVAL=30
+CHECK_IP_URL="https://check.torproject.org/api/ip"
+RENEW_DELAY=5
+STATUS_FILE="/tmp/status"
 
 while true; do
   sleep $SLEEP_INTERVAL
@@ -12,9 +15,19 @@ while true; do
   PAGE_CONTENT=$(curl -x $SOCKS_PROXY -s $ONION_URL)
 
   if [[ $? -eq 0 ]]; then
-    (echo "$PAGE_CONTENT" | grep -qm1 $GREP_PATTERN) || (printf 'AUTHENTICATE ""\r\nSIGNAL NEWNYM\r\n' | nc $TOR_COMMAND_PORT)
-    echo "IP address renewed"
+    echo "$PAGE_CONTENT" | grep -qm1 $GREP_PATTERN
+    if [[ $? -ne 0 ]]; then
+      echo "BLOCKED" > $STATUS_FILE
+      BEFORE_IP=$(curl -s --socks5 $SOCKS_PROXY $CHECK_IP_URL | jq -r '.IP')
+      printf 'AUTHENTICATE ""\r\nSIGNAL NEWNYM\r\n' | nc $TOR_COMMAND_PORT
+      sleep $RENEW_DELAY
+      AFTER_IP=$(curl -s --socks5 $SOCKS_PROXY $CHECK_IP_URL | jq -r '.IP')
+      echo "IP address renewed from $BEFORE_IP to $AFTER_IP"
+    else
+      echo "OK" > $STATUS_FILE
+    fi
   else
     echo "Curl command failed. Retrying..."
+    echo "FAIL" > $STATUS_FILE
   fi
 done
--- tor.Dockerfile
+++ tor.Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 
-RUN apk --no-cache --update --upgrade add tor curl grep netcat-openbsd \
+RUN apk --no-cache --update --upgrade add tor curl grep netcat-openbsd jq \
     && mv /etc/tor/torrc.sample  /etc/tor/torrc \
     && sed -i \
         -e 's/#SOCKSPort 192.168.0.1:9100/SOCKSPort 0.0.0.0:9050/g' \
@@ -11,9 +11,10 @@ COPY renew-tor.sh /etc/tor/
 RUN chmod +x /etc/tor/renew-tor.sh
 
 USER tor
+RUN echo "OK" > /tmp/status
 ENTRYPOINT tor & /etc/tor/renew-tor.sh
 
 EXPOSE 9050/tcp
 
-HEALTHCHECK --interval=60s --timeout=15s --start-period=20s \
-    CMD curl -s --socks5 127.0.0.1:9050 'https://check.torproject.org/api/ip' | grep -qm1 -E '"IsTor"\s*:\s*true'
+HEALTHCHECK --interval=30s --timeout=1s --start-period=5s \
+    CMD test "$(cat /tmp/status)" = "OK"
