b2bc85ae7eb36a40e602be6627ad93f8a3da88ec
Ben Adrian Sarmiento <me@bensarmiento.com>
2023-08-31 21:52:40 +0200

    new website (#55)
    * new website
    
    * bump version

--- src/services/planetscale.ts
+++ src/services/planetscale.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 export class PlanetScaleCache {
-	private prisma: PrismaClient;
+	public prisma: PrismaClient;
 
 	constructor() {
 		this.prisma = new PrismaClient();
