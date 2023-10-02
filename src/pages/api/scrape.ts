622a7d99417403401e7a1519b6ddb080f67fa2ff
Ben Sarmiento <ben.sarmiento@forto.com>
2023-10-03 00:22:10 +0200

    fix torrentio, add param for replacement

--- src/pages/api/scrape.ts
+++ src/pages/api/scrape.ts
@@ -3,7 +3,7 @@ import { ScrapeResponse, generateScrapeJobs } from '@/services/scrapeJobs';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ScrapeResponse>) {
-	const { imdbId, scrapePassword } = req.query;
+	const { imdbId, scrapePassword, replaceOldScrape } = req.query;
 	if (process.env.SCRAPE_API_PASSWORD && scrapePassword !== process.env.SCRAPE_API_PASSWORD) {
 		res.status(403).json({
 			status: 'error',
@@ -18,6 +18,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 	}
 
 	await new PlanetScaleCache().delete(imdbId);
-	await generateScrapeJobs(imdbId.toString().trim());
+	await generateScrapeJobs(imdbId.toString().trim(), replaceOldScrape === 'true');
 	res.status(200).json({ status: 'success' });
 }
