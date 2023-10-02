8aa535303aa7bdddd32535924b567583f9dbbf60
Ben Sarmiento <ben.sarmiento@forto.com>
2023-10-02 22:23:23 +0200

    fix logic

--- src/pages/api/movieupdater2.ts
+++ src/pages/api/movieupdater2.ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 		});
 		return;
 	}
-	let startFromHere = startFrom as string ?? '';
+	let startFromHere = (startFrom as string) ?? '';
 	for (const imdbId of imdbIds) {
 		if (startFromHere && imdbId !== startFromHere) continue;
 		startFromHere = '';
--- src/utils/mediaId.ts
+++ src/utils/mediaId.ts
@@ -1,5 +1,6 @@
 import { ParsedMovie, ParsedShow } from '@ctrl/video-filename-parser';
 
+// Prefixes a number with a character and leading zero if necessary
 const prefix = (char: string, num: number): string => `${char}${num < 10 ? '0' : ''}${num}`;
 
 export const getMediaId = (
@@ -8,29 +9,55 @@ export const getMediaId = (
 	systemOnlyId = true,
 	tvShowTitleOnly = false
 ) => {
+	// Check if info is string and assign titleId accordingly
 	const titleId: string = typeof info === 'string' ? info : info.title;
-	if (mediaType === 'movie')
+
+	// If media type is movie, return formatted string
+	if (mediaType === 'movie') {
 		return `${systemOnlyId ? titleId.toLocaleLowerCase() : titleId}${
 			typeof info !== 'string' && info.year ? ` (${info.year})` : ''
 		}`;
-	const { title, seasons, fullSeason, isMultiSeason, episodeNumbers } = info as ParsedShow;
-	const titleStr = systemOnlyId ? title.toLocaleLowerCase() : title;
-	if (tvShowTitleOnly) {
-		return titleStr;
 	}
+
+	// If info is string or only the title of the TV show is required, return titleId
+	if (typeof info === 'string' || tvShowTitleOnly) {
+		return systemOnlyId ? titleId.toLocaleLowerCase() : titleId;
+	}
+
+	// Destructure info of type ParsedShow
+	const { title, seasons, fullSeason, isMultiSeason, episodeNumbers } = info as ParsedShow;
+
+	// Format title string
+	const titleStr = systemOnlyId ? title.toLocaleLowerCase() : title;
+
+	// If seasons are not present or empty, return title string
 	if (!seasons || seasons.length === 0) return titleStr;
-	const season = systemOnlyId ? 's' : 'S';
-	const episode = systemOnlyId ? 'e' : 'E';
+
+	// Define season and episode prefixes
+	const seasonPrefix = systemOnlyId ? 's' : 'S';
+	const episodePrefix = systemOnlyId ? 'e' : 'E';
+
+	// Handle multi-season case
 	if (isMultiSeason) {
-		return `${titleStr} ${prefix(season, Math.min(...seasons))}${'-'}${prefix(
-			season,
+		return `${titleStr} ${prefix(seasonPrefix, Math.min(...seasons))}-${prefix(
+			seasonPrefix,
 			Math.max(...seasons)
 		)}`;
-	} else if (fullSeason) {
-		return `${titleStr} ${prefix(season, Math.min(...seasons))}`;
 	}
-	return `${titleStr} ${prefix(season, Math.min(...seasons))}${prefix(
-		episode,
-		episodeNumbers[0]
-	)}`;
+
+	// Handle full season case
+	else if (fullSeason) {
+		return `${titleStr} ${prefix(seasonPrefix, Math.min(...seasons))}`;
+	}
+
+	// Handle single episode case
+	else if (episodeNumbers && episodeNumbers.length > 0) {
+		return `${titleStr} ${prefix(seasonPrefix, Math.min(...seasons))}${prefix(
+			episodePrefix,
+			episodeNumbers[0]
+		)}`;
+	}
+
+	// Default case, return title and season without episode
+	return `${titleStr} ${prefix(seasonPrefix, Math.min(...seasons))}`;
 };
