72bb7c52d99bcaa8c4c10110d9588a8652f5ae63
Ben Sarmiento <ben.sarmiento@forto.com>
2023-11-15 14:41:23 +0100

    Add clean up function

--- src/pages/show/[imdbid]/[seasonNum].tsx
+++ src/pages/show/[imdbid]/[seasonNum].tsx
@@ -5,7 +5,7 @@ import { deleteMagnet, uploadMagnet } from '@/services/allDebrid';
 import { SearchApiResponse, SearchResult } from '@/services/mediasearch';
 import { addHashAsMagnet, deleteTorrent, getTorrentInfo, selectFiles } from '@/services/realDebrid';
 import { instantCheckInAd, instantCheckInRd, wrapLoading } from '@/utils/instantChecks';
-import { getSelectableFiles, isVideoOrSubs } from '@/utils/selectable';
+import { getSelectableFiles, isVideo } from '@/utils/selectable';
 import { searchToastOptions } from '@/utils/toastOptions';
 import { withAuth } from '@/utils/withAuth';
 import axios from 'axios';
@@ -169,7 +169,7 @@ const TvSearch: FunctionComponent<TvSearchProps> = ({
 			const response = await getTorrentInfo(rdKey, id.substring(3));
 			if (response.filename === 'Magnet') return; // no files yet
 
-			const selectedFiles = getSelectableFiles(response.files.filter(isVideoOrSubs)).map(
+			const selectedFiles = getSelectableFiles(response.files.filter(isVideo)).map(
 				(file) => file.id
 			);
 			if (selectedFiles.length === 0) {
