56ff6a2dfeb3ee6f0dbfc25d3d75595b0937b263
Ben Sarmiento <ben.sarmiento@forto.com>
2023-12-16 02:43:10 +0100

    Hide icons if no use

---
+++ src/utils/deleteList.ts
@@ -0,0 +1,21 @@
+import { UserTorrent } from '@/types/userTorrent';
+import toast from 'react-hot-toast';
+import { AsyncFunction, runConcurrentFunctions } from './batch';
+import { magnetToastOptions } from './toastOptions';
+
+export async function deleteFilteredTorrents(
+	torrentList: UserTorrent[],
+	wrapDeleteFn: (t: UserTorrent) => AsyncFunction<void>
+) {
+	const toDelete = torrentList.map(wrapDeleteFn);
+	const [results, errors] = await runConcurrentFunctions(toDelete, 5, 500);
+	if (errors.length) {
+		toast.error(`Error deleting ${errors.length} torrents`, magnetToastOptions);
+	}
+	if (results.length) {
+		toast.success(`Deleted ${results.length} torrents`, magnetToastOptions);
+	}
+	if (!errors.length && !results.length) {
+		toast('No torrents to delete', magnetToastOptions);
+	}
+}
