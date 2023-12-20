a83f2d719442ed35a1de668ede5f7e8fcc718ac1
Ben Sarmiento <ben.sarmiento@forto.com>
2023-12-20 20:31:46 +0100

    Add bare functions to rd api

--- src/pages/api/watch/[app].ts
+++ src/pages/api/watch/[app].ts
@@ -1,9 +1,9 @@
-import { bareUnrestrictLink } from '@/services/realDebrid';
+import { unrestrictLink } from '@/services/realDebrid';
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { app, token, link } = req.query;
-	const resp = await bareUnrestrictLink(token as string, link as string);
+	const resp = await unrestrictLink(token as string, link as string, true);
 	res.redirect(307, `${app}://${resp.download.replace('https://', '')}`);
 };
 
