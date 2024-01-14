65721a73ec31a9c805b613a3d88b4943a12f5309
Ben Sarmiento <ben.sarmiento@forto.com>
2024-01-14 13:10:59 +0100

    Change lookbehind test

--- src/utils/lookbehind.ts
+++ src/utils/lookbehind.ts
@@ -1,9 +1,10 @@
+import assert from "assert";
+
 export function supportsLookbehind() {
 	try {
-		// Test a regex with a lookbehind assertion
-		new RegExp('(?<=@)\\w+');
-
-		// If no exception is thrown, lookbehind is supported
+		if ('$foo %foo foo'.replace(/(\$)foo/g, '$1bar') !== '$bar %foo foo') return false;
+		if ('$foo %foo foo'.replace(/(?<=\$)foo/g, 'bar') !== '$bar %foo foo') return false;
+		if ('$foo %foo foo'.replace(/(?<!\$)foo/g, 'bar') !== '$foo %bar bar') return false;
 		return true;
 	} catch (e) {
 		// If an exception is thrown, lookbehind is not supported
