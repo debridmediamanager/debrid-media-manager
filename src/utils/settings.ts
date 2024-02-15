c69679fa9c59e49a94fa79ef122eb3eac2b0666c
Ben Sarmiento <ben.sarmiento@forto.com>
2024-02-15 23:28:40 +0100

    Size filtering

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,16 +1,21 @@
 import Swal from 'sweetalert2';
 
 export const defaultPlayer = 'web/rd';
-export const defaultSize = '5';
+export const defaultMovieSize = '0';
+export const defaultEpisodeSize = '0';
 
 export const showSettings = async () => {
 	const storedPlayer = window.localStorage.getItem('player') || defaultPlayer;
 	const isPlayer = (player: string) =>
 		storedPlayer === player ? `value="${player}" selected` : `value="${player}"`;
 
-	const storedMaxSize = window.localStorage.getItem('maxSize') || defaultSize;
-	const isSize = (size: string) =>
-		storedMaxSize === size ? `value="${size}" selected` : `value="${size}"`;
+	const movieMaxSize = window.localStorage.getItem('movieMaxSize') || defaultMovieSize;
+	const isMovieSize = (size: string) =>
+		movieMaxSize === size ? `value="${size}" selected` : `value="${size}"`;
+
+	const episodeMaxSize = window.localStorage.getItem('episodeMaxSize') || defaultEpisodeSize;
+	const isEpisodeSize = (size: string) =>
+		episodeMaxSize === size ? `value="${size}" selected` : `value="${size}"`;
 
 	const { value: formValues } = await Swal.fire({
 		title: '⚙️ Settings',
@@ -42,12 +47,28 @@ export const showSettings = async () => {
 
 			<div name="divider" class="py-2"></div>
 
-            <label for="dmm-max-size">Biggest file size to show:</label>
-            <select id="dmm-max-size" class="swal2-input">
-                <option ${isSize('5')}>5 GB</option>
-				<option ${isSize('15')}>15 GB</option>
-				<option ${isSize('60')}>60 GB</option>
-				<option ${isSize('0')}>Doesn't matter</option>
+            <label for="dmm-movie-max-size">Biggest movie size to show:</label>
+            <select id="dmm-movie-max-size" class="swal2-input">
+				<option ${isMovieSize('1')}>1 GB</option>
+				<option ${isMovieSize('3')}>3 GB</option>
+				<option ${isMovieSize('5')}>5 GB</option>
+				<option ${isMovieSize('15')}>15 GB</option>
+				<option ${isMovieSize('30')}>30 GB</option>
+				<option ${isMovieSize('60')}>60 GB</option>
+				<option ${isMovieSize('0')}>Biggest available</option>
+			</select>
+
+			<div name="divider" class="py-2"></div>
+
+            <label for="dmm-episode-max-size">Biggest episode size to show:</label>
+            <select id="dmm-episode-max-size" class="swal2-input">
+                <option ${isEpisodeSize('0.1')}>100 MB</option>
+				<option ${isEpisodeSize('0.3')}>300 MB</option>
+				<option ${isEpisodeSize('0.5')}>500 MB</option>
+				<option ${isEpisodeSize('1')}>1 GB</option>
+				<option ${isEpisodeSize('3')}>3 GB</option>
+				<option ${isEpisodeSize('5')}>5 GB</option>
+				<option ${isEpisodeSize('0')}>Biggest available</option>
 			</select>
 
 			<div name="divider" class="py-2"></div>
@@ -62,13 +83,18 @@ export const showSettings = async () => {
 		preConfirm: () => {
 			return {
 				player: (document.getElementById('dmm-player') as HTMLInputElement).value,
-				maxSize: (document.getElementById('dmm-max-size') as HTMLInputElement).value,
+				movieMaxSize: (document.getElementById('dmm-movie-max-size') as HTMLInputElement)
+					.value,
+				episodeMaxSize: (
+					document.getElementById('dmm-episode-max-size') as HTMLInputElement
+				).value,
 			};
 		},
 	});
 
 	if (formValues) {
 		window.localStorage.setItem('player', formValues.player);
-		window.localStorage.setItem('maxSize', formValues.maxSize);
+		window.localStorage.setItem('movieMaxSize', formValues.movieMaxSize);
+		window.localStorage.setItem('episodeMaxSize', formValues.episodeMaxSize);
 	}
 };
