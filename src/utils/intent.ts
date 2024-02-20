6abb6e613dd3619a415fdc1ea4653ce9f433e6a1
Ben Sarmiento <ben.sarmiento@forto.com>
2024-02-20 02:30:26 +0100

    Fix type issue

--- src/utils/intent.ts
+++ src/utils/intent.ts
@@ -30,10 +30,9 @@ export const getInstantIntent = async (
 				const biggestFileIdx = torrent.files.findIndex((f) => f.id === biggestFile.id);
 				link = torrent.links[biggestFileIdx] ?? torrent.links[0];
 			} else {
-				const intFileId = parseInt(fileId);
 				const fileIdx = torrent.files
 					.filter((f) => f.selected)
-					.findIndex((f) => f.id === intFileId);
+					.findIndex((f) => f.id === fileId);
 				link = torrent.links[fileIdx] ?? torrent.links[0];
 			}
 
