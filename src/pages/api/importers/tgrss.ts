2bc7c073fb976eed4e0eb892d186eac3217c79d3
Ben Sarmiento <ben.sarmiento@forto.com>
2024-03-30 14:32:06 +0100

    tgx rss

--- src/pages/api/importers/tgrss.ts
+++ src/pages/api/importers/tgrss.ts
@@ -12,74 +12,150 @@ const tgItemUrl = (id: string) => `https://torrentgalaxy.to/torrent/${id}`;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ScrapeResponse>) {
 	while (true) {
-		const scrapesMap = new Map<string, any>();
+		const toSave: { key: string; value: ScrapeSearchResult[] }[] = [];
 		try {
 			const response: any = await axios.get(tgRssUrl());
-			let torrentsToProcess = response.data.results || [];
-			torrentsToProcess = torrentsToProcess.filter((e: any) => e.i && e.i.startsWith('tt'));
-			console.log(`[tgx] found ${torrentsToProcess.length} torrents`);
-			try {
-				for (const torrent of torrentsToProcess) {
-					const item: any = await axios.get(tgItemUrl(torrent.pk));
-					const result = processStream(item.data);
-					if (torrent.c === 'Movies') {
-						if (scrapesMap.has(`movie:${torrent.i}`)) {
-							scrapesMap.get(`movie:${torrent.i}`).push(result);
-						} else {
-							scrapesMap.set(`movie:${torrent.i}`, [result]);
-						}
-					} else {
-						let seasonNum: number | null = null;
-						const seasonMatch =
-							result.title.match(/S(\d{1,2})E?/i) ||
-							result.title.match(/S[a-z]+n\s?(\d{1,2})/i) ||
-							result.title.match(/(\d{1,2})x\d{1,2}/i);
-						if (seasonMatch && seasonMatch[1]) {
-							seasonNum = parseInt(seasonMatch[1], 10);
-						} else {
-							console.warn('No season match, setting to 1', result.title);
-							seasonNum = 1; // Default to season 1 if no match is found
-						}
-						if (scrapesMap.has(`tv:${torrent.i}:${seasonNum}`)) {
-							scrapesMap.get(`tv:${torrent.i}:${seasonNum}`).push(result);
-						} else {
-							scrapesMap.set(`tv:${torrent.i}:${seasonNum}`, [result]);
-						}
-					}
+			const rssText = response.data;
+			const categoryRegex = /<category>(.*?)<\/category>/g;
+			// Regex to find URLs within the comments tag and extract the ID
+			const urlRegex = /<comments>https:\/\/torrentgalaxy\.to\/torrent\/(\d+)/g;
+			const categoryMatches = rssText.match(categoryRegex);
+			const urlMatches = rssText.match(urlRegex);
+			for (let i = 0; i < categoryMatches.length; i++) {
+				const category = categoryMatches[i].replace(/<\/?category>/g, '');
+				if (!category.startsWith('Movies') && !category.startsWith('TV')) continue;
+				// get item and pass to tgItemUrl
+				const itemUrl = urlMatches[i].replace(/<\/?comments>/g, '');
+				const item = await processItem(itemUrl);
+				if (!item) continue;
+
+				if (category.startsWith('Movies')) {
+					// console.log(`[tgx] found ${category} ${itemUrl}`);
+					// console.log('Movie item', item);
+					toSave.push({
+						key: `movie:${item.imdb}`,
+						value: [
+							{
+								title: item.title.trim(),
+								fileSize: computeFileSize(item.size),
+								hash: item.hash,
+							},
+						],
+					});
 				}
-			} catch (e) {
-				console.log(`[tgx] failed (${e})`);
+				if (category.startsWith('TV')) {
+					// console.log(`[tgx] found ${category} ${itemUrl}`);
+					let seasonNum: number | null = null;
+					const seasonMatch =
+						item.title.match(/S(\d{1,2})E?/i) ||
+						item.title.match(/S[a-z]+n\s?(\d{1,2})/i) ||
+						item.title.match(/(\d{1,2})x\d{1,2}/i);
+					if (seasonMatch && seasonMatch[1]) {
+						seasonNum = parseInt(seasonMatch[1], 10);
+					} else {
+						console.warn('No season match, setting to 1', item.title);
+						seasonNum = 1; // Default to season 1 if no match is found
+					}
+					toSave.push({
+						key: `tv:${item.imdb}:${seasonNum}`,
+						value: [
+							{
+								title: item.title.trim(),
+								fileSize: computeFileSize(item.size),
+								hash: item.hash,
+							},
+						],
+					});
+					// console.log('TV item', item);
+				}
 			}
+			for (const save of toSave) {
+				const url = `https://debridmediamanager.com/${save.key
+					.replaceAll(':', '/')
+					.replaceAll('tv/', 'show/')}`;
+				console.log(`[tgx] saving ${url}`, save.value);
+				await db.saveScrapedTrueResults(save.key, save.value, true);
+			}
+			await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
 		} catch (e) {
 			console.log(`[tgx] failed (${e})`);
 		}
-		const toSave: { key: string; value: ScrapeSearchResult[] }[] = [];
-		scrapesMap.forEach((value: ScrapeSearchResult[], key: string) => {
-			toSave.push({ key, value });
-		});
-		for (const save of toSave) {
-			await db.saveScrapedTrueResults(save.key, save.value, true);
-		}
-		scrapesMap.forEach((scrapes, key) => {
-			const url = `https://debridmediamanager.com/${key
-				.replaceAll(':', '/')
-				.replaceAll('tv/', 'show/')}`;
-			console.log(url, scrapes);
-		});
 	}
 }
 
 interface TGItem {
-	eid: string;
-	name: string;
-	info_hash: string;
-	size: number;
+	title: string;
+	size: string;
+	hash: string;
+	imdb: string;
 }
 
-function processStream(item: TGItem): ScrapeSearchResult {
-	return {
-		title: item.name,
-		fileSize: item.size / 1024 / 1024,
-		hash: item.info_hash.toLowerCase(),
-	};
+async function processItem(url: string): Promise<TGItem | null> {
+	try {
+		// console.log(`[tgx] processing ${url}`);
+		const response = await axios.get(url);
+		const itemText = response.data;
+		console.log(`[tgx] got ${itemText.length} bytes`);
+
+		const titleRegex = /<title>TGx:(.*)<\/title>/g;
+		const hashRegex = /watercache\.nanobytes\.org\/get\/([a-f0-9]+)/g;
+		// Total Size:</b></div><div class='tpcell'>3.96 GB</div>
+		const sizeRegex = /Total Size:<\/b><\/div><div class='tpcell'>([^<]+)<\/div>/g;
+		const imdbRegex = /www\.imdb\.com\/title\/(tt\d+)/g;
+
+		const titleMatches = itemText.match(titleRegex);
+		// console.log(`[tgx] titleMatches: ${titleMatches}`);
+		const hashMatches = itemText.match(hashRegex);
+		// console.log(`[tgx] hashMatches: ${hashMatches}`);
+		const sizeMatches = itemText.match(sizeRegex);
+		// console.log(`[tgx] sizeMatches: ${sizeMatches}`);
+		const imdbMatches = itemText.match(imdbRegex);
+		// if (titleMatches === null || hashMatches === null || sizeMatches === null || imdbMatches === null) {
+		// 	console.log(`[tgx] failed to match ${url}`);
+		// 	return null;
+		// }
+		// console.log(`[tgx] imdbMatches: ${imdbMatches}`);
+
+		const title = titleMatches[0]
+			.replace(/<\/?title>/g, '')
+			.replace('TGx:', '')
+			.trim();
+		const hash = hashMatches[0]
+			.replace('watercache.nanobytes.org/get/', '')
+			.toLocaleLowerCase();
+		const size = sizeMatches[0]
+			.replace("Total Size:</b></div><div class='tpcell'>", '')
+			.replace('</div>', '');
+		const imdb = imdbMatches[0].replace('www.imdb.com/title/', '');
+
+		return {
+			title,
+			hash,
+			size,
+			imdb,
+		};
+	} catch (e) {
+		console.log(`[tgx] failed to process ${url} (${e})`);
+		return null;
+	}
+}
+
+function computeFileSize(size: string): number {
+	const sizeMatch = size.match(/([\d.]+)\s([MGT])B/);
+	if (!sizeMatch) {
+		console.log(`[tgx] failed to match size ${size}`);
+		return 0;
+	}
+	const num = parseFloat(sizeMatch[1]);
+	const unit = sizeMatch[2];
+	switch (unit) {
+		case 'M':
+			return num;
+		case 'G':
+			return num * 1024;
+		case 'T':
+			return num * 1024 * 1024;
+		default:
+			return 0;
+	}
 }
