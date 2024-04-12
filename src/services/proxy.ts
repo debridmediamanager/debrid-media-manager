2db1d41337a161f4a928ca1b0d0f22c36b45f88d
Ben Sarmiento <ben.sarmiento@forto.com>
2024-04-12 02:25:09 +0200

    Use proxy

--- src/services/proxy.ts
+++ src/services/proxy.ts
@@ -5,16 +5,8 @@ import UserAgent from 'user-agents';
 export const createAxiosInstance = (agent: SocksProxyAgent) => {
 	return axios.create({
 		httpAgent: agent,
+		httpsAgent: agent,
 		headers: {
-			accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-			'accept-language': 'en-US,en;q=0.5',
-			'accept-encoding': 'gzip, deflate, br',
-			connection: 'keep-alive',
-			'sec-fetch-dest': 'document',
-			'sec-fetch-mode': 'navigate',
-			'sec-fetch-site': 'same-origin',
-			'sec-fetch-user': '?1',
-			'upgrade-insecure-requests': '1',
 			'user-agent': new UserAgent().toString(),
 		},
 		timeout: parseInt(process.env.REQUEST_TIMEOUT || '3000', 10),
