10a0f0cb9df513b908a3c145cd19f3f45e7b0ebe
Ben Sarmiento <ben.sarmiento@forto.com>
2024-04-12 18:19:07 +0200

    add rutor

--- src/services/tvScraper.ts
+++ src/services/tvScraper.ts
@@ -13,6 +13,7 @@ import { scrapeIDope } from './idope';
 import { scrapeMagnetDL } from './magnetdl';
 import { ScrapeSearchResult, flattenAndRemoveDuplicates, sortByFileSize } from './mediasearch';
 import { PlanetScaleCache } from './planetscale';
+import { scrapeRuTor } from './rutor';
 import { scrapeSolidTorrent } from './solidtorrent';
 import { scrapeTorrentGalaxy } from './tgx';
 
@@ -43,6 +44,7 @@ async function scrapeAll(
 		scrapeMagnetDL(finalQuery.replaceAll('"', ''), targetTitle, years, airDate),
 		scrapeTorrentGalaxy(finalQuery.replaceAll('"', ''), targetTitle, years, airDate),
 		scrapeGloTorrents(finalQuery.replaceAll('"', ''), targetTitle, years, airDate),
+		scrapeRuTor(finalQuery.replaceAll('"', ''), targetTitle, years, airDate),
 	]);
 }
 
