dcee911a6b9ed2a277077eabd89cd522854b6312
Ben Adrian Sarmiento <me@bensarmiento.com>
2024-06-12 03:25:15 +0200

    Support filtering by hash

--- src/utils/quickSearch.ts
+++ src/utils/quickSearch.ts
@@ -1,11 +1,18 @@
 import { SearchResult } from '@/services/mediasearch';
 import { UserTorrent } from '@/torrent/userTorrent';
 
+const HASH_LENGTH_MIN_THRESHOLD = 8;
+
 // given a list, filter by query and paginate
 export function applyQuickSearch(query: string, unfiltered: UserTorrent[]) {
 	let regexFilters: RegExp[] = [];
-	for (const q of query.split(' ')) {
+	for (let q of query.split(' ')) {
 		try {
+			if (q.startsWith('magnet:?')) {
+				const hash = q.match(/xt=urn:btih:([a-zA-Z0-9]{8,40})/);
+				if (hash && hash.length >= 2) regexFilters.push(new RegExp(hash[1], 'i'));
+				continue;
+			}
 			regexFilters.push(new RegExp(q, 'i'));
 		} catch (error) {
 			continue;
@@ -13,7 +20,7 @@ export function applyQuickSearch(query: string, unfiltered: UserTorrent[]) {
 	}
 	return query
 		? unfiltered.filter((t) =>
-				regexFilters.every((regex) => regex.test(t.filename) || regex.test(t.id))
+				regexFilters.every((regex) => regex.test(t.filename) || regex.test(t.id) || (regex.toString().length >= HASH_LENGTH_MIN_THRESHOLD+3 && regex.test(t.hash)))
 			)
 		: unfiltered;
 }
