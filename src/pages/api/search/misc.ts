0828684dd0f7a56c9465c06451ecf734f60c7db1
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-09-24 19:24:28 +0200

    Temp fix for ddos issue

--- src/pages/api/search/misc.ts
+++ src/pages/api/search/misc.ts
@@ -10,34 +10,33 @@ type SearchMiscResponseCache = {
 const responses: Record<string, SearchMiscResponseCache> = {};
 
 const handler: NextApiHandler = async (req, res) => {
-	const { keyword } = req.query;
-	const keywordStr = keyword as string;
+	// const { keyword } = req.query;
+	// const keywordStr = keyword as string;
 
-	const mdblist = new MdbList();
-	let topLists;
-	if (!keywordStr) {
-		topLists = await mdblist.searchLists('latest');
-	} else {
-		topLists = await mdblist.searchLists(keywordStr);
-	}
+	// const mdblist = new MdbList();
+	// let topLists;
+	// if (!keywordStr) {
+	// 	topLists = await mdblist.searchLists('latest');
+	// } else {
+	// 	topLists = await mdblist.searchLists(keywordStr);
+	// }
 
-	let rng = lcg(new Date().getTime() / 1000 / 60 / 10);
-	topLists = shuffle(topLists, rng).slice(0, 1);
-	// topLists = topLists.slice(0, 1);
+	// let rng = lcg(new Date().getTime() / 1000 / 60 / 10);
+	// topLists = shuffle(topLists, rng).slice(0, 1);
 
 	const response: SearchMiscResponse = {};
-	for (const list of topLists) {
-		const itemsResponse = await mdblist.listItems(list.id);
-		response[list.name] = itemsResponse
-			.filter((item) => item.imdb_id)
-			.slice(0, 16)
-			.map((item) => `${list.mediatype}:${item.imdb_id}:${item.title}`);
-	}
+	// for (const list of topLists) {
+	// 	const itemsResponse = await mdblist.listItems(list.id);
+	// 	response[list.name] = itemsResponse
+	// 		.filter((item) => item.imdb_id)
+	// 		.slice(0, 16)
+	// 		.map((item) => `${list.mediatype}:${item.imdb_id}:${item.title}`);
+	// }
 
-	responses[keywordStr] = {
-		lastUpdated: new Date().getTime(),
-		response,
-	};
+	// responses[keywordStr] = {
+	// 	lastUpdated: new Date().getTime(),
+	// 	response,
+	// };
 
 	res.status(200).json(response);
 };
