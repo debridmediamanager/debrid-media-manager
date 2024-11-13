216d47778c9e677956634e9bbc5a3fd5ae00377b
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-11-13 23:53:51 +0100

    Fix posters

---
+++ src/pages/api/poster.ts
@@ -0,0 +1,38 @@
+import { getTmdbKey } from '@/utils/freekeys';
+import { TmdbResponse } from '@/utils/tmdb';
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { imdbid } = req.query;
+
+	if (!imdbid || typeof imdbid !== 'string') {
+		return res.status(400).json({ error: 'IMDB ID is required' });
+	}
+
+	const mdblistKey = process.env.MDBLIST_KEY;
+	const getTmdbInfo = (imdbId: string) =>
+		`https://api.themoviedb.org/3/find/${imdbId}?api_key=${getTmdbKey()}&external_source=imdb_id`;
+	const getMdbInfo = (imdbId: string) =>
+		`https://mdblist.com/api/?apikey=${mdblistKey}&i=${imdbId}`;
+
+	try {
+		const tmdbResp = await axios.get<TmdbResponse>(getTmdbInfo(imdbid));
+		const movieResult = tmdbResp.data.movie_results[0];
+		const tvResult = tmdbResp.data.tv_results[0];
+		const posterPath = movieResult?.poster_path || tvResult?.poster_path;
+
+		if (posterPath) {
+			return res.json({ url: `https://image.tmdb.org/t/p/w500${posterPath}` });
+		}
+
+		const mdbResp = await axios.get(getMdbInfo(imdbid));
+		if (mdbResp.data.poster && mdbResp.data.poster.startsWith('http')) {
+			return res.json({ url: mdbResp.data.poster });
+		}
+
+		return res.status(404).json({ error: 'Poster not found' });
+	} catch (error) {
+		return res.status(404).json({ error: 'Poster not found' });
+	}
+}
