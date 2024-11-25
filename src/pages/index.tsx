fa3979d88eac258d7c8794bf49cfd07e75c3b479
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-11-25 13:23:07 +0100

    nofollow, noindex

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,6 +119,7 @@ function IndexPage() {
 		<div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 p-4">
 			<Head>
 				<title>Debrid Media Manager - Home</title>
+				<meta name="robots" content="index, nofollow" />
 			</Head>
 			<Logo />
 			<Toaster position="bottom-right" />
