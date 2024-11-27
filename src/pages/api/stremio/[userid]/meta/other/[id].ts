ab88bf268922e181bcb3808f9609c83d1d460123
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-11-27 08:44:55 +0100

    Wrap with try catch

---
+++ src/pages/api/stremio/[userid]/meta/other/[id].ts
@@ -0,0 +1,23 @@
+import { getDMMTorrent } from '@/utils/castCatalogHelper';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { userid, id } = req.query;
+	if (typeof userid !== 'string' || typeof id !== 'string') {
+		return res.status(400).json({ error: 'Invalid "userid" or "id" query parameter' });
+	}
+	const torrentID = id.replaceAll(/^dmm:/g, '').replaceAll(/\.json$/g, '');
+
+	try {
+		const result = await getDMMTorrent(userid as string, torrentID);
+		res.setHeader('access-control-allow-origin', '*');
+		if ('error' in result) {
+			return res.status(result.status).json({ error: result.error });
+		}
+
+		res.status(result.status).json(result.data);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: `Failed to get DMM torrent: ${error}` });
+	}
+}
