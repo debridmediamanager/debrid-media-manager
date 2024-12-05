bc54a5d5bd634a725679ca5496be23353544bafe
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-12-05 22:50:13 +0100

    Fix typescript errors

--- src/components/LibraryHelpText.tsx
+++ src/components/LibraryHelpText.tsx
@@ -1,14 +1,14 @@
 interface LibraryHelpTextProps {
-  helpText: string;
-  onHide: () => void;
+	helpText: string;
+	onHide: () => void;
 }
 
 export default function LibraryHelpText({ helpText, onHide }: LibraryHelpTextProps) {
-  if (!helpText || helpText === 'hide') return null;
-  
-  return (
-    <div className="bg-blue-900 py-0.5 text-xs" onClick={onHide}>
-      💡 {helpText}
-    </div>
-  );
+	if (!helpText || helpText === 'hide') return null;
+
+	return (
+		<div className="bg-blue-900 py-0.5 text-xs" onClick={onHide}>
+			💡 {helpText}
+		</div>
+	);
 }
--- src/components/LibraryTableHeader.tsx
+++ src/components/LibraryTableHeader.tsx
@@ -1,56 +1,57 @@
 interface LibraryTableHeaderProps {
-  sortBy: {
-    column: 'id' | 'filename' | 'title' | 'bytes' | 'progress' | 'status' | 'added';
-    direction: 'asc' | 'desc';
-  };
-  onSort: (column: 'id' | 'filename' | 'title' | 'bytes' | 'progress' | 'status' | 'added') => void;
-  filteredListLength: number;
-  selectedTorrentsSize: number;
+	sortBy: {
+		column: 'id' | 'filename' | 'title' | 'bytes' | 'progress' | 'status' | 'added';
+		direction: 'asc' | 'desc';
+	};
+	onSort: (
+		column: 'id' | 'filename' | 'title' | 'bytes' | 'progress' | 'status' | 'added'
+	) => void;
+	filteredListLength: number;
+	selectedTorrentsSize: number;
 }
 
 export default function LibraryTableHeader({
-  sortBy,
-  onSort,
-  filteredListLength,
-  selectedTorrentsSize,
+	sortBy,
+	onSort,
+	filteredListLength,
+	selectedTorrentsSize,
 }: LibraryTableHeaderProps) {
-  return (
-    <tr className="whitespace-nowrap border-b border-gray-700 text-xs">
-      <th
-        className="w-8 min-w-8 max-w-8 px-0.5 py-1 text-gray-300"
-        onClick={() => onSort('id')}
-      >
-        Select
-        {selectedTorrentsSize ? ` (${selectedTorrentsSize})` : ''}{' '}
-        {sortBy.column === 'id' && (sortBy.direction === 'asc' ? '↑' : '↓')}
-      </th>
-      <th
-        className="w-[500px] min-w-96 max-w-[500px] px-1 py-2 text-gray-300"
-        onClick={() => onSort('title')}
-      >
-        Title ({filteredListLength}){' '}
-        {sortBy.column === 'title' && (sortBy.direction === 'asc' ? '↑' : '↓')}
-      </th>
-      <th
-        className="w-20 min-w-20 max-w-20 px-1 py-2 text-gray-300"
-        onClick={() => onSort('bytes')}
-      >
-        Size {sortBy.column === 'bytes' && (sortBy.direction === 'asc' ? '↑' : '↓')}
-      </th>
-      <th
-        className="w-20 min-w-20 max-w-20 px-1 py-2 text-gray-300"
-        onClick={() => onSort('progress')}
-      >
-        Status{' '}
-        {sortBy.column === 'progress' && (sortBy.direction === 'asc' ? '↑' : '↓')}
-      </th>
-      <th
-        className="w-24 min-w-24 max-w-28 px-1 py-2 text-gray-300"
-        onClick={() => onSort('added')}
-      >
-        Added {sortBy.column === 'added' && (sortBy.direction === 'asc' ? '↑' : '↓')}
-      </th>
-      <th className="w-24 min-w-24 max-w-28 px-1 py-2 text-gray-300">Actions</th>
-    </tr>
-  );
+	return (
+		<tr className="whitespace-nowrap border-b border-gray-700 text-xs">
+			<th
+				className="w-8 min-w-8 max-w-8 px-0.5 py-1 text-gray-300"
+				onClick={() => onSort('id')}
+			>
+				Select
+				{selectedTorrentsSize ? ` (${selectedTorrentsSize})` : ''}{' '}
+				{sortBy.column === 'id' && (sortBy.direction === 'asc' ? '↑' : '↓')}
+			</th>
+			<th
+				className="w-[500px] min-w-96 max-w-[500px] px-1 py-2 text-gray-300"
+				onClick={() => onSort('title')}
+			>
+				Title ({filteredListLength}){' '}
+				{sortBy.column === 'title' && (sortBy.direction === 'asc' ? '↑' : '↓')}
+			</th>
+			<th
+				className="w-20 min-w-20 max-w-20 px-1 py-2 text-gray-300"
+				onClick={() => onSort('bytes')}
+			>
+				Size {sortBy.column === 'bytes' && (sortBy.direction === 'asc' ? '↑' : '↓')}
+			</th>
+			<th
+				className="w-20 min-w-20 max-w-20 px-1 py-2 text-gray-300"
+				onClick={() => onSort('progress')}
+			>
+				Status {sortBy.column === 'progress' && (sortBy.direction === 'asc' ? '↑' : '↓')}
+			</th>
+			<th
+				className="w-24 min-w-24 max-w-28 px-1 py-2 text-gray-300"
+				onClick={() => onSort('added')}
+			>
+				Added {sortBy.column === 'added' && (sortBy.direction === 'asc' ? '↑' : '↓')}
+			</th>
+			<th className="w-24 min-w-24 max-w-28 px-1 py-2 text-gray-300">Actions</th>
+		</tr>
+	);
 }
