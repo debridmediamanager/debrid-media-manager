28c5e9fa63e8b1cf3caddaf33df87f75dad5b992
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-12-07 22:48:01 +0100

    Handle scan if torrent is already in library

--- src/pages/show/[imdbid]/[seasonNum].tsx
+++ src/pages/show/[imdbid]/[seasonNum].tsx
@@ -335,8 +335,13 @@ const TvSearch: FunctionComponent = () => {
 				const batch = nonAvailableResults.slice(i, i + 3);
 				await Promise.all(
 					batch.map(async (result) => {
-						await addRd(result.hash);
-						await deleteRd(result.hash);
+						if (`rd:${result.hash}` in hashAndProgress) {
+							await deleteRd(result.hash);
+							await addRd(result.hash);
+						} else {
+							await addRd(result.hash);
+							await deleteRd(result.hash);
+						}
 					})
 				);
 			}
