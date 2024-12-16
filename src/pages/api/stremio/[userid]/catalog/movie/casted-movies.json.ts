5ba9c0b38b4fff8526f07d3a071a857c6f082700
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-12-16 18:18:55 +0100

    set cacheMaxAge to 0

--- src/pages/api/stremio/[userid]/catalog/movie/casted-movies.json.ts
+++ src/pages/api/stremio/[userid]/catalog/movie/casted-movies.json.ts
@@ -30,5 +30,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	res.setHeader('access-control-allow-origin', '*');
 	res.status(200).json({
 		metas: movies,
+		cacheMaxAge: 0,
 	});
 }
