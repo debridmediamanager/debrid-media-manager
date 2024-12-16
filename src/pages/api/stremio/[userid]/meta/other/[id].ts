103af5f3a0047ede9b3b5ef6ee6f98058cb2dadc
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-12-16 02:02:42 +0100

    Redo token generation in dmm cast

--- src/pages/api/stremio/[userid]/meta/other/[id].ts
+++ src/pages/api/stremio/[userid]/meta/other/[id].ts
@@ -1,24 +1,56 @@
+import { Repository } from '@/services/planetscale';
+import { getToken } from '@/services/realDebrid';
 import { getDMMTorrent } from '@/utils/castCatalogHelper';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+const db = new Repository();
 
 // gets information about a torrent (viewing your library)
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { userid, id } = req.query;
 	if (typeof userid !== 'string' || typeof id !== 'string') {
-		return res.status(400).json({ error: 'Invalid "userid" or "id" query parameter' });
+		res.status(400).json({
+			status: 'error',
+			errorMessage: 'Invalid "userid" or "id" query parameter',
+		});
+		return;
 	}
+
+	if (req.method === 'OPTIONS') {
+		return res.status(200).end();
+	}
+
 	const torrentID = id.replaceAll(/^dmm:/g, '').replaceAll(/\.json$/g, '');
 
+	const profile = await db.getCastProfile(userid);
+	if (!profile) {
+		res.status(500).json({ error: `Failed to get Real-Debrid profile for user ${userid}` });
+		return;
+	}
+
+	const response = await getToken(
+		profile.clientId,
+		profile.clientSecret,
+		profile.refreshToken,
+		false
+	);
+	if (!response) {
+		res.status(500).json({ error: `Failed to get Real-Debrid token for user ${userid}` });
+		return;
+	}
+
 	try {
-		const result = await getDMMTorrent(userid as string, torrentID);
+		const result = await getDMMTorrent(userid as string, torrentID, response.access_token);
 		res.setHeader('access-control-allow-origin', '*');
 		if ('error' in result) {
-			return res.status(result.status).json({ error: result.error });
+			res.status(result.status).json({ error: result.error });
+			return;
 		}
 
 		res.status(result.status).json(result.data);
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: `Failed to get DMM torrent: ${error}` });
+		return;
 	}
 }
