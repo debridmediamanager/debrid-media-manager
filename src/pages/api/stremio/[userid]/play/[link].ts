6049a53d2d39ad85df08c0ac2ad8933cc4bec03e
Ben Adrian Sarmiento <ben.sarmiento@enter.de>
2024-12-16 01:09:14 +0100

    wrap try catch when unrestricting link

--- src/pages/api/stremio/[userid]/play/[link].ts
+++ src/pages/api/stremio/[userid]/play/[link].ts
@@ -4,10 +4,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const db = new Repository();
 
-export async function HEAD(req: NextApiRequest, res: NextApiResponse) {
-	res.status(200).end();
-}
-
 // Unrestrict and play a link
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { userid, link } = req.query;
@@ -15,29 +11,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		return res.status(400).json({ error: 'Invalid "userid" or "link" query parameter' });
 	}
 
-	const profile = await db.getCastProfile(userid as string);
-	if (!profile) {
-		return res.status(401).json({ error: 'Go to DMM and connect your RD account' });
-	}
-	const response = await getToken(
-		profile.clientId,
-		profile.clientSecret,
-		profile.refreshToken,
-		false
-	);
-	if (!response) {
-		return res.status(500).json({ error: 'Go to DMM and connect your RD account' });
-	}
-	const ipAddress = (req.headers['cf-connecting-ip'] as string) ?? req.socket.remoteAddress;
-	const unrestrict = await unrestrictLink(
-		response.access_token,
-		`https://real-debrid.com/d/${link.substring(0, 13)}`,
-		ipAddress,
-		false
-	);
-	if (!unrestrict) {
-		return res.status(500).json({ error: 'Failed to unrestrict link' });
-	}
+	try {
+		const profile = await db.getCastProfile(userid as string);
+		if (!profile) {
+			return res.status(401).json({ error: 'Go to DMM and connect your RD account' });
+		}
+		const response = await getToken(
+			profile.clientId,
+			profile.clientSecret,
+			profile.refreshToken,
+			false
+		);
+		if (!response) {
+			return res.status(500).json({ error: 'Go to DMM and connect your RD account' });
+		}
+		const ipAddress = (req.headers['cf-connecting-ip'] as string) ?? req.socket.remoteAddress;
+		const unrestrict = await unrestrictLink(
+			response.access_token,
+			`https://real-debrid.com/d/${link.substring(0, 13)}`,
+			ipAddress,
+			false
+		);
+		if (!unrestrict) {
+			return res.status(500).json({ error: 'Failed to unrestrict link' });
+		}
 
-	res.redirect(unrestrict.download);
+		res.redirect(unrestrict.download);
+	} catch (error: any) {
+		console.error(error);
+		res.status(500).json({ error: `Failed to play link: ${error}` });
+	}
 }
